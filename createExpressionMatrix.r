aad835131b86b8f75e825c0bb8a96c25d2a9e46c
Dylan Maillart <42812669+dmaillart@users.noreply.github.com>
2021-09-28 16:48:48 -0400

    Add files via upload

---
+++ createExpressionMatrix.r
@@ -0,0 +1,5 @@
+temp = list.files(pattern="*.count")
+myfiles = lapply(temp, read.csv)
+matrix <- bind_cols(myfiles, .id="column_label")
+expressionMatrix <- select(matrix, -c(3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65))
+View(expressionMatrix)
